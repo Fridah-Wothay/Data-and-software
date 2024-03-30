222079e2f4cf8e6e79c8a46dd87cd05f310eedca
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-03-31 00:13:35 +0300

    added stacks file

---
+++ stacks.py
@@ -0,0 +1,19 @@
+fruits = []
+fruits.append("Guava")
+fruits.append("oranges")
+fruits.append("Kiwi")
+print(fruits)
+
+# accessing stack
+fruits=['Guava','oranges','Kiwi']
+print(fruits[2])
+
+# adding items in stack
+fruits=['Guava','oranges','Kiwi']
+fruits.append('ovacado')
+print(fruits)
+
+#remove items in stack
+fruits=['Guava','oranges','Kiwi','ovacado']
+fruits.pop()
+print(fruits)
