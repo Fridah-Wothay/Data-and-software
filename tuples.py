879912fb20eff6444a6ce7c62782a0f83f4bdf40
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-03-31 00:17:50 +0300

    added tuples file

---
+++ tuples.py
@@ -0,0 +1,15 @@
+Tupple = ("apple","Mellon","oranges","Bananas")
+print(Tupple[3])
+
+# Add item in a tupple
+x=Tupple+('Guava','Kiwi')
+print(x)
+
+
+# Remove item in a tupple
+x = list(Tupple)
+print(x)
+y = x.remove("Mellon")
+print(x)
+y=tuple(Tupple)
+print(Tupple)
